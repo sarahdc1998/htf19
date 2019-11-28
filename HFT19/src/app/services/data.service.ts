1f78ba2c84b274e2a06d603cf109bcc41309de1e
Sarah De Cock <sarah.decock2@student.odisee.be>
2019-11-28 10:35:20 +0100

    add components

--- HFT19/src/app/services/data.service.ts
+++ HFT19/src/app/services/data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  baseUrl = "https://htf.zinderlabs.com/";
+  token = "915e69a1245cfac52524f58fa9f6c9e7";
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public getBanks() {
+    this.http.get(`${this.baseUrl}/banks`, { headers: { Authorization: `Bearer ${this.token}` }});
+  }
+
+  public getBank(id) {
+
+  }
+
+  public getAccounts() {
+
+  }
+
+  public  getAccount(id) {
+
+  }
 }
+
+
